7fbcb27ffab0902beb17b65f50e845092fe1b008
Rohit060708Drak <darkeaglemovies@gmail.com>
2025-09-27 22:01:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,34 +2,35 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from pyrogram import Client, idle
+from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "techvj_login",  # session name
+            "techvj login",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             plugins=dict(root="TechVJ"),
-            workers=1,            # lower workers to prevent flood
+            workers=50,
             sleep_threshold=10
         )
 
+      
     async def start(self):
+            
         await super().start()
-        print("Bot Started Powered By @VJ_Botz")
+        print('Bot Started Powered By @VJ_Botz')
 
     async def stop(self, *args):
+
         await super().stop()
-        print("Bot Stopped Bye")
+        print('Bot Stopped Bye')
 
-# Create bot instance
-bot = Bot()
+Bot().run()
 
-# Start bot safely
-bot.start()
-idle()  # Keeps the bot running without triggering FloodWait
-bot.stop()
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
